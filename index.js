6730215acff145a7ca8bb85402e018cdcf765e6e
Pedro Garay <125901731+PedroMG4@users.noreply.github.com>
2025-04-23 22:24:18 -0300

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use((req, res, next) => {
 app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.send('🚀 Backend de prueba funcionando');
+  res.send('Backend de prueba funcionando');
 });
 
 app.post('/enviar-pedido', (req, res) => {
